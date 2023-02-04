80a4b93f6acc3fcd9a2f84d545e94fc3a6d898c7
Marius <40984378+MariusBrummer@users.noreply.github.com>
2023-02-04 15:34:00 +0100

    Added lecture3 files

---
+++ lecture3/tasks/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+tasks = ["task1", "task2", "task3"]
+# Create your views here.
+def index(request):
+    return render(request, 'tasks/index.html', {
+        "tasks": tasks
+        })
+
+
+
+
